0f0f44b69ecc6c15bc1709fee4049b43863c30c1
Margaux Cavagna <margaux.cavagna@hotmail.fr>
2019-05-14 08:33:41 +0200

    Capteur + Squelettes

---
+++ Squelette.cpp
@@ -0,0 +1,80 @@
+/*************************************************************************
+                           ${file_base}  -  description
+                             -------------------
+    d�but                : ${date}
+    copyright            : (C) ${year} par ${user}
+*************************************************************************/
+
+//---------- R�alisation de la classe <${file_base}> (fichier ${file_name}) --
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include syst�me
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "${file_base}.h"
+
+//------------------------------------------------------------- Constantes
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types priv�s
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- M�thodes publiques
+// type ${file_base}::M�thode ( liste de param�tres )
+// Algorithme :
+//
+//{
+//} //----- Fin de M�thode
+
+
+//------------------------------------------------- Surcharge d'op�rateurs
+${file_base} & ${file_base}::operator = ( const ${file_base} & un${file_base} )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+${file_base}::${file_base} ( const ${file_base} & un${file_base} )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <${file_base}>" << endl;
+#endif
+} //----- Fin de ${file_base} (constructeur de copie)
+
+
+${file_base}::${file_base} ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <${file_base}>" << endl;
+#endif
+} //----- Fin de ${file_base}
+
+
+${file_base}::~${file_base} ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <${file_base}>" << endl;
+#endif
+} //----- Fin de ~${file_base}
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- M�thodes prot�g�es
+
+//------------------------------------------------------- M�thodes priv�es
