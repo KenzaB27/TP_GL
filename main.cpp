cc5087d02b3887aee9a6bf9b58619d1286564571
Dreami_ <swyx3r@gmail.com>
2019-05-30 19:49:32 +0200

    test dans le main (sorry)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include "Catalogue.h"
+#include "Lecture.h"
+
 int main()
 {
+	/*
+
+	TESTS EN COURS -----------
+
+
+	Catalogue c;
+	Lecture l;
+
+	l.InitTypeGaz("../Fichiers/gazTest.csv");
+	l.Parcourir(c, "../Fichiers/valuesTest.csv");
+
+	unordered_map<IdCatalogue, vector<MesureGaz>> map;
+	map = c.getMap();
+
+	for (auto x : map)
+	{
+		cout << x.first << endl;
+		cout << "bbb" << endl;
+	}
+
+	//cout << c << endl;
+	system("pause");
+	*/
 	return 0;
 }
