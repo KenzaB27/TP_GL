d6d6a8bf9a14e0ee581038e03be337b8de033b3f
Dreami_ <swyx3r@gmail.com>
2019-05-31 00:26:17 +0200

    Init Seuils fonctionnel

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
 #include <stdlib.h>
+#include "Lecture.h"
 
 int main()
 {
-	/*
-
-	TESTS EN COURS ----------- */
-
-
-	Catalogue* c = new Catalogue();
-	Lecture l;
-
-	l.InitTypeGaz("../../Fichiers/gazTest.csv");
-	l.Parcourir(c, "../../Fichiers/fichier1000.csv");
-
-	unordered_map<IdCatalogue, vector<MesureGaz>> map;
-
-	cout << *c << endl;
-
-	system("pause");
-
+	
 	return 0;
 }
